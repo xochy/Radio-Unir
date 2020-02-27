7338e75f671d500ec71dd744cdb29924ca7f23fd
xochy <xochydeather@gmail.com>
2020-02-27 00:07:17 -0600

    regreso cambio

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Radio GENARO
+                    Radio UNIR
                 </div>
 
                 <div class="links">
